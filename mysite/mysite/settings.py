b0d9f92b722d90e688ee3444638b4317e77bc14b
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-01-02 17:22:55 +0000

    Secret Key uses Environment Variable

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=2^hx(dq=6$m^%7&z7-x$=n_&if_cdy0+j2-5sntfth9ksmzwe'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
